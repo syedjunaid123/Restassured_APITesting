413e5bb5ddf6692c1bc0ee3e0d38b0b5a35de9ec
Syed Junaid <syedjunaid123@gmail.com>
2020-11-01 13:12:59 +0530

    First Push with Scenario 1

---
+++ src/test/java/apiautomation/Tests.java
@@ -0,0 +1,23 @@
+package apiautomation;
+
+import org.testng.annotations.Test;
+
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class Tests {
+
+	@Test
+	void scenario_01() {
+		given()
+			.when()
+				.get("http://dummy.restapiexample.com/api/v1/employees")
+			.then()
+				.statusCode(200)
+//				.body("data.profile_image", hasItems(""));
+		
+				.body("data.profile_image", hasItems(blankOrNullString()));
+	}
+}
