94aaa881b6f0ceb7729bd02f4ca9ab7183f72ea2
Syed Junaid <syedjunaid123@gmail.com>
2020-11-01 13:21:48 +0530

    removed commented code

--- src/test/java/apiautomation/Tests.java
+++ src/test/java/apiautomation/Tests.java
@@ -9,15 +9,14 @@ import static org.hamcrest.Matchers.*;
 
 public class Tests {
 
+	@SuppressWarnings("unchecked")
 	@Test
 	void scenario_01() {
 		given()
 			.when()
 				.get("http://dummy.restapiexample.com/api/v1/employees")
 			.then()
-				.statusCode(200)
-//				.body("data.profile_image", hasItems(""));
-		
+				.statusCode(200)		
 				.body("data.profile_image", hasItems(blankOrNullString()));
 	}
 }
