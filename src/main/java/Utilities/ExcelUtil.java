f119f6b5fa7e9e7e986d550ff734bbc4dcd880d6
Syed Junaid <syedjunaid123@gmail.com>
2020-11-02 00:17:27 +0530

    Fetching Employee ID from Excel

--- src/main/java/Utilities/ExcelUtil.java
+++ src/main/java/Utilities/ExcelUtil.java
@@ -1,43 +1,14 @@
 package Utilities;
 
-
-import java.io.IOException;
 import java.util.Random;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtil {
-//	static XSSFWorkbook workbook;
-//	static XSSFSheet sheet;
-//	
-//	static String projectPath = System.getProperty("user.dir");
-//	static String excelPath = projectPath+"/src/test/resources/Employee_Id_data.xlsx";
-//	
-//	public ExcelUtil(){
-//		try {
-//			workbook = new XSSFWorkbook(excelPath);
-//			sheet = workbook.getSheet("employees_id's");
-//			
-//		} catch (Exception e) {
-//			
-//			e.printStackTrace();
-//		}
-//		
-//		
-//		
-//	}
-//	
-//	public static void getData() {
-//		Random random_employee = new Random(); 
-//		int random_employee_id = random_employee.nextInt(24) + 1;
-//		int employee_id = (int)sheet.getRow(random_employee_id).getCell(0).getNumericCellValue();
-//		System.out.println(random_employee_id);
-//		System.out.println(employee_id);
-//	}
-//	
 	
-	
-	public static void getCellValue() {
+	public int getCellValue() {
+		
+		int employee_id = 0;
 		try {
 			Random random_employee = new Random(); 
 			String projectPath = System.getProperty("user.dir");
@@ -47,20 +18,15 @@ public class ExcelUtil {
 			XSSFSheet sheet = workbook.getSheet("employees_id's");
 			
 			int random_employee_id = random_employee.nextInt(24) + 1;
-			int employee_id = (int)sheet.getRow(random_employee_id).getCell(0).getNumericCellValue();
-			System.out.println(random_employee_id);
-			System.out.println(employee_id);
+			employee_id = (int)sheet.getRow(random_employee_id).getCell(0).getNumericCellValue();
+
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.out.println(e.getCause());
 			e.printStackTrace();
 		}
-		return;
+		return employee_id;
+	}
 
-	}
-	public static void main(String[] args) {
-		getCellValue();
-		
-	}
 }
--- src/test/java/apiautomation/Scenario2_Tests.java
+++ src/test/java/apiautomation/Scenario2_Tests.java
@@ -8,20 +8,23 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
-public class Scenario2_Tests extends GetConfigProperties {
+import Utilities.ExcelUtil;
+
+public class Scenario2_Tests extends GetConfigProperties{
 
 	Properties props = this.gettingproperties();
-
-
+	ExcelUtil axe = new ExcelUtil();
+	
 	@Test
 	void scenario_02() {
 		given()
 			.when()
-				.get(props.getProperty("API_Individual_Employee"))
+				.get(props.getProperty("API_Individual_Employee")+ axe.getCellValue())
 		.then()
 			.assertThat()
 				.statusCode(200)
 				.body(matchesJsonSchemaInClasspath("EmployeeSchema.json"), containsString("Successfully! Record has been fetched."));
 
 	}
+
 }
